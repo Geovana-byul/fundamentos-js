ddc2da906ddf7c0643f02e05d92d05530427e611
Byul <geovanaconceicao817@gmail.com>
2022-06-02 02:06:21 -0300

    parte 2 conversoes e afins

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleano
+
+//conversão implícita 
+const numero = 456;
+const numerostring = Number("456");
+
+
+
+//Number()
+//String()
+
+console.log(numero + numerostring)
+
+//conversão explícita
---
+++ truthy-falsy.js
@@ -0,0 +1,22 @@
+//boolean
+
+const usuarioLogado = true
+const contapaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+//null ==> vazio ou nada
+//undefined
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof numero);
+console.log(typeof texto);
